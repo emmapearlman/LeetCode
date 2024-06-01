6f67bf8ca6b0cddc1ec55ce716f886c15e2a211c
Emma Pearlman <emma.pearlman@gmail.com>
2024-06-01 17:47:46 +0100

    Add project files.

---
+++ LeetCode/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LeetCode;
+
+Console.WriteLine("Hello, World!");
+Solution solution = new Solution();
+Console.WriteLine(solution.MergeAlternately("abc", "pqr"));
+Console.WriteLine(solution.MergeAlternately("ab", "pqrs"));
+Console.WriteLine(solution.MergeAlternately("abcd", "pq"));
+Console.ReadLine();
+
---
+++ LeetCode/Solution.cs
@@ -0,0 +1,43 @@
+﻿namespace LeetCode
+{
+    using System.Linq;
+    using System.Text;
+
+    public class Solution
+    {
+        public string MergeAlternately(string word1, string word2)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            string[] word1Array = word1.ToCharArray().Select(c => c.ToString()).ToArray();
+            string[] word2Array = word2.ToCharArray().Select(c => c.ToString()).ToArray();
+            if (word2Array.Length > word1Array.Length)
+            {
+                word2Array = word2Array.Take(word1Array.Length).ToArray();
+            }
+            if (word1Array.Length > word2Array.Length)
+            {
+                word1Array = word1Array.Take(word2Array.Length).ToArray();
+            }
+            for (int i = 0; i <= word1Array.Length - 1; i++)
+            {
+                sb.Append(word1Array[i]);
+                sb.Append(word2Array[i]);
+
+            }
+            if (word2.Length > word1.Length)
+            {
+                var diff = word2.Length - word1.Length;
+                var extra = word2.Substring(word1.Length, diff);
+                sb.Append(extra.ToString());
+            }
+            if (word1.Length > word2.Length)
+            {
+                var diff = word1.Length - word2.Length;
+                var extra = word1.Substring(word2.Length, diff);
+                sb.Append(extra.ToString());
+            }
+            return sb.ToString();
+        }
+    }
+}
