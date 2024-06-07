1eb1dfb90d7dcfc03eb362eae6fe226cc87b4959
emmapearlman <emma.pearlman@gmail.com>
2024-06-07 07:13:35 +0100

    get hand of straights

--- LeetCodeTests/LeetCodeTests.cs
+++ LeetCodeTests/LeetCodeTests.cs
@@ -6,12 +6,12 @@ namespace LeetCodeTests
     {
         private readonly Solution solution = new Solution();
 
-        [TestCase("abc", "pqr","apbqcr")]
+        [TestCase("abc", "pqr", "apbqcr")]
         [TestCase("ab", "pqrs", "apbqrs")]
         [TestCase("abcd", "pq", "apbqcd")]
         public void MergeAlternatelyReturnsMergedString(string word1, string word2, string expected)
         {
-            var actual =solution.MergeAlternately(word1, word2);
+            var actual = solution.MergeAlternately(word1, word2);
             Assert.That(actual, Is.EqualTo(expected));
         }
 
@@ -27,7 +27,7 @@ namespace LeetCodeTests
         [TestCase(null)]
         public void ScoreOfStringReturnsZeroisWordIsNullOrempty(string? input)
         {
-            var expected =0;
+            var expected = 0;
             var actual = solution.ScoreOfString(input);
             Assert.That(actual, Is.EqualTo(expected));
         }
@@ -36,16 +36,16 @@ namespace LeetCodeTests
         public void ReverseStringReturnsReversedString()
         {
             var expected = new char[] { 'o', 'l', 'l', 'e', 'h' };
-            var actual = solution.ReverseString(new char[] { 'h', 'e','l','l','o' });
+            var actual = solution.ReverseString(new char[] { 'h', 'e', 'l', 'l', 'o' });
             Assert.That(actual, Is.EqualTo(expected));
         }
 
-        [TestCase("coaching", "coding", 4 )]
+        [TestCase("coaching", "coding", 4)]
         [TestCase("abcde", "a", 0)]
         [TestCase("z", "abcde", 5)]
         public void AppendCharactersReturnsNumberofCharactersSpecified(string s, string t, int expected)
         {
-            var actual = solution.AppendCharacters( s, t );
+            var actual = solution.AppendCharacters(s, t);
             Assert.That(actual, Is.EqualTo(expected));
         }
 
@@ -56,5 +56,23 @@ namespace LeetCodeTests
             var actual = solution.AppendCharacters(s, t);
             Assert.That(actual, Is.EqualTo(0));
         }
+
+        [TestCase(new object[] { 1, 2, 3, 6, 2, 3, 4, 7, 8 }, 3)]
+        public void IsNStraightHandReturnsTrueIfMultipleHandsReturned(object[] input, int groupSize)
+        {
+            var nums = input.Select(o => (int)o).ToArray();
+            var actual = solution.IsNStraightHand(nums, groupSize);
+            Assert.That(actual, Is.True);
+        }
+
+        [TestCase(new object[] { 1, 2, 3, 4, 5 }, 4)]
+        public void IsNStraightHandReturnsFalseIfMultipleHandsCannnotBeReturned(object[] input, int groupSize)
+        {
+            var nums = input.Select(o => (int)o).ToArray();
+            var actual = solution.IsNStraightHand(nums, groupSize);
+            Assert.That(actual, Is.False);
+        }
+
+
     }
 }
